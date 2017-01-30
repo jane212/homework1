2e4250fd7742c19d920b5585202ef848c0f94c99
Tingting <thuang1@iastate.edu>
2017-01-30 12:14:48 -0600

    parser improve

--- python_project/parser.py
+++ python_project/parser.py
@@ -12,8 +12,8 @@ f = open('streamdata.csv','w')
 
 for i in range (1,100):
 	z = root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()[i]
-	content = z.getchildren()[0].text+","+z.getchildren()[1].text
+	content = z.getchildren()[0].text+","+z.getchildren()[1].text+"\n"
 	f.write(content)
-	f.write('\n')
+
 
 f.close()
