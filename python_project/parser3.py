84a5b71cae04b799394299518bc1abad67c17576
Tingting <thuang1@iastate.edu>
2017-01-31 19:51:10 -0600

    parser for part 3

---
+++ python_project/parser3.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import urllib2
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("stream.xml")
+
+root = tree.getroot()
+
+z = root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()
+
+length = len(z)
+
+f = open('streamdata.csv','w')
+
+for i in range (1,length):
+	x = root.getchildren()[2].getchildren()[0].getchildren()[3].getchildren()[i]
+	content = x.getchildren()[0].text+","+x.getchildren()[1].text+"\n"
+	f.write(content)
+
+f.close()
